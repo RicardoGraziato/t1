d12369d59fd9f1984e3025a9557f1338ab4f8ac3
Eduarda Bertiz <eduardabertiz@hotmail.com>
2022-09-23 18:13:27 -0300

    att

--- Main.java
+++ Main.java
@@ -7,16 +7,15 @@ public class Main{
 
         do{
             score1 = input.nextDouble();
-            if(score1<0 || score1>10) System.out.print("nota invalida");
+            if(score1<0 || score1>10) System.out.print("nota invalida\n");
         }while(score1<0 || score1>10);
 
         do{
             score2 = input.nextDouble();
-            if(score2<0 || score2>10) System.out.print("nota invalida");
+            if(score2<0 || score2>10) System.out.print("nota invalida\n");
         }while(score2<0 || score2>10);
 
         media = (score1+score2)/2.0;
         System.out.printf("media = %.2f",media);
-	System.out.print("");
     }
 }
