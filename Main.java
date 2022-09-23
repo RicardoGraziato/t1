d5004c087193992435866553b064bc4150a3e024
Guilherme de Moraes Machado Pereira Silva <guimps1909@gmail.com>
2022-09-23 13:42:02 -0300

    Codigo fonte

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double score1,score2,media;
+
+        do{
+            score1 = input.nextDouble();
+            if(score1<0 || score1>10) System.out.print("nota invalida");
+        }while(score1<0 || score1>10);
+
+        do{
+            score2 = input.nextDouble();
+            if(score2<0 || score2>10) System.out.print("nota invalida");
+        }while(score2<0 || score2>10);
+
+        media = (score1+score2)/2.0;
+        System.out.printf("media = %.2f",media);
+    }
+}
